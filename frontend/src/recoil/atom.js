4b5d0022144828876cb89ad0039c4fd900077a23
hyoseung2000 <hyoseung.lee2000@gmail.com>
2025-03-18 05:48:20 +0900

    init: recoil 설정

---
+++ frontend/src/recoil/atom.js
@@ -0,0 +1,12 @@
+//전역 상태관리 > recoil
+//대문자 카멜케이스 네이밍
+
+// import { atom } from 'recoil';
+
+// //유저 아이디
+// const UserId = atom({
+//   key: 'userid',
+//   default: 1,
+// });
+
+// export { UserId };
