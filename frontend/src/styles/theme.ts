90b497cdc520ddd771c6c8b392b40cf2bbf1f819
hyoseung2000 <hyoseung.lee2000@gmail.com>
2025-03-18 05:48:36 +0900

    init: 전역 스타일링 설정

---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,169 @@
+import { css, DefaultTheme } from 'styled-components';
+
+const colors = {
+  Red: '#C1121F',
+  DarkRed: '#780000',
+  Navy: '#0239A6',
+  MainBlue: '#0062FF',
+  SkyBlue: '#0062FF',
+  Blue: '#669BBC',
+  DarkYellow: '#D6B842',
+  Yellow: '#F7D95F',
+  Gray0: '#191919',
+  Gray1: '#3D3D3D',
+  Gray2: '#CFCED3',
+  Gray3: '#E8E8F4',
+  Gray4: '#EEEEFA',
+  Gray5: '#F3F3F3',
+  Beige: '#FFF4DE',
+  White: '#FFFFFF',
+};
+
+const fonts = {
+  Head0: css`
+    font-family: 'Pretendard';
+    font-size: 4rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 130%;
+  `,
+  Head1: css`
+    font-family: 'Pretendard';
+    font-size: 3.2rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 130%;
+  `,
+  Head2: css`
+    font-family: 'Pretendard';
+    font-size: 3rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 130%;
+  `,
+  Title1: css`
+    font-family: 'Pretendard';
+    font-size: 2.6rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 130%;
+  `,
+  Title2: css`
+    font-family: 'Pretendard';
+    font-size: 2.4rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 130%;
+  `,
+  Title3: css`
+    font-family: 'Pretendard';
+    font-size: 2rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 130%;
+  `,
+  Title4: css`
+    font-family: 'Pretendard';
+    font-size: 1.8rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 130%;
+  `,
+  Title5: css`
+    font-family: 'Pretendard';
+    font-size: 1.6rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 130%;
+  `,
+  Title6: css`
+    font-family: 'Pretendard';
+    font-size: 1.4rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 130%;
+  `,
+  Title7: css`
+    font-family: 'Pretendard';
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 130%;
+  `,
+  Body0: css`
+    font-family: 'Pretendard';
+    font-size: 2.2rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 130%;
+  `,
+  Body1: css`
+    font-family: 'Pretendard';
+    font-size: 1.6rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 130%;
+  `,
+  Body2: css`
+    font-family: 'Pretendard';
+    font-size: 1.4rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 130%;
+  `,
+  Body3: css`
+    font-family: 'Pretendard';
+    font-size: 1.3rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 130%;
+  `,
+  Body4: css`
+    font-family: 'Pretendard';
+    font-size: 1.2rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 130%;
+  `,
+  Body5: css`
+    font-family: 'Pretendard';
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 130%;
+  `,
+  Body6: css`
+    font-family: 'Pretendard';
+    font-size: 0.9rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 130%;
+  `,
+  Body7: css`
+    font-family: 'Pretendard';
+    font-size: 0.8rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 130%;
+  `,
+  Body8: css`
+    font-family: 'Pretendard';
+    font-size: 0.7rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 130%;
+  `,
+  Body9: css`
+    font-family: 'Pretendard';
+    font-size: 0.6rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 130%;
+  `,
+};
+
+const theme: DefaultTheme = {
+  colors,
+  fonts,
+};
+export default theme;
