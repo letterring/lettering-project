05fd73325f294a482839de443806059259bd1327
김지수 <comjisu0311@naver.com>
2025-03-18 18:14:59 +0900

    hotifx config: "cors config 설정"

--- backend/src/main/java/com/example/lettering/config/CorsConfig.java
+++ backend/src/main/java/com/example/lettering/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList(
                 "Authorization",
