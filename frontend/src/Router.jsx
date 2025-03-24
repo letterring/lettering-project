f06a6d73342c8c4a6d278311851ecbf7274adecb
0ys <0ysgong@gmail.com>
2025-03-24 10:10:36 +0900

    init: 받는 사람 라우터 페이지 설정정

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -3,6 +3,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
 import ScrollToTop from './components/common/ScrollToTop';
+import DearHomePage from './pages/dear/Homepage';
+import DearLandingPage from './pages/dear/LandingPage';
+import DearPostDetailPage from './pages/dear/PostcardDetailPage';
+import DearPostcardPage from './pages/dear/PostcardPage';
 import ErrorPage from './pages/ErrorPage';
 import HomePage from './pages/sender/Homepage';
 
@@ -17,6 +21,10 @@ const Router = () => {
             <Route path="/" element={<HomePage />} />
 
             {/* dear */}
+            <Route path="/dear" element={<DearLandingPage />} />
+            <Route path="/dear/home" element={<DearHomePage />} />
+            <Route path="/dear/postcard" element={<DearPostcardPage />} />
+            <Route path="/dear/postdetail" element={<DearPostDetailPage />} />
 
             {/* common */}
             <Route path="*" element={<ErrorPage />} />
