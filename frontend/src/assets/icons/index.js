00f883d06858409500a8fae45b69085827f6fc1d
이효승 <julia_2000@naver.com>
2025-03-24 18:30:58 +0900

    S12P21A203-91 feat: 공통 헤더 컴포넌트 구현

--- frontend/src/assets/icons/index.js
+++ frontend/src/assets/icons/index.js
@@ -1,2 +1,3 @@
 export { ReactComponent as IcBack } from './ic_back.svg';
 export { ReactComponent as IcMenu } from './ic_menu.svg';
+export { ReactComponent as IcMenuSmall } from './ic_menu_small.svg';
