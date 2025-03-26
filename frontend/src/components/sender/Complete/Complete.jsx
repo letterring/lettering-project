c90df696ab0a40c0340af7519e013ba6eb1258dd
hnl9912 <hnl9912@naver.com>
2025-03-26 16:01:26 +0900

    chore: 전송완료 화면 관련 경로 수정

--- frontend/src/components/sender/Complete/Complete.jsx
+++ frontend/src/components/sender/Complete/Complete.jsx
@@ -1,44 +1,52 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import CompleteImg from '../../../assets/images/sender/TransmissionComplete.png';
+import PaperBackground from '/src/assets/background2.png';
+import CompleteImage from '/src/assets/images/sender/TransmissionComplete.png';
 
-const CompletePage = () => {
+const Complete = () => {
   return (
-    <Wrapper>
-      <Image src={CompleteImg} alt="전송 완료 이미지" />
-      <Title>전송 완료!!</Title>
+    <CompleteWrapper $Background={PaperBackground}>
+      <img src={CompleteImage} alt="전송 완료 이미지" />
+      <CompleteText>전송 완료!!</CompleteText>
       <SubText>키링을 태그하면 도착한 편지를 바로 확인할 수 있어요!</SubText>
-    </Wrapper>
+    </CompleteWrapper>
   );
 };
 
-export default CompletePage;
+export default Complete;
 
-const Wrapper = styled.div`
+const CompleteWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-image: url(${({ $Background }) => $Background});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh;
-  background-color: ${({ theme }) => theme.colors.Background};
+
   padding: 2rem;
-  text-align: center;
+  box-sizing: border-box;
+
+  img {
+    width: 50%;
+    max-width: 24rem;
+    margin-bottom: 2.5rem;
+  }
 `;
 
-const Image = styled.img`
-  width: 18rem;
-  height: auto;
-  margin-bottom: 3rem;
-`;
-
-const Title = styled.div`
-  ${({ theme }) => theme.fonts.Saeum3};
+const CompleteText = styled.div`
   color: ${({ theme }) => theme.colors.Gray1};
+  ${({ theme }) => theme.fonts.Saeum0};
   margin-bottom: 1rem;
 `;
 
 const SubText = styled.div`
-  ${({ theme }) => theme.fonts.Body3};
-  color: ${({ theme }) => theme.colors.Gray3};
+  color: ${({ theme }) => theme.colors.Gray4};
+  ${({ theme }) => theme.fonts.Body2};
+  text-align: center;
 `;
--- frontend/src/pages/sender/CompletePage.jsx
+++ frontend/src/pages/sender/CompletePage.jsx
@@ -1,52 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
 
-import PaperBackground from '/src/assets/background2.png';
-import CompleteImage from '/src/assets/images/sender/TransmissionComplete.png';
+import Complete from '../../components/sender/Complete/Complete';
 
-const Complete = () => {
-  return (
-    <CompleteWrapper $Background={PaperBackground}>
-      <img src={CompleteImage} alt="전송 완료 이미지" />
-      <CompleteText>전송 완료!!</CompleteText>
-      <SubText>키링을 태그하면 도착한 편지를 바로 확인할 수 있어요!</SubText>
-    </CompleteWrapper>
-  );
+const CompletePage = () => {
+  return <Complete />;
 };
 
-export default Complete;
-
-const CompleteWrapper = styled.div`
-  width: 100%;
-  height: 100vh;
-  background-image: url(${({ $Background }) => $Background});
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  padding: 2rem;
-  box-sizing: border-box;
-
-  img {
-    width: 50%;
-    max-width: 24rem;
-    margin-bottom: 2.5rem;
-  }
-`;
-
-const CompleteText = styled.div`
-  color: ${({ theme }) => theme.colors.Gray1};
-  ${({ theme }) => theme.fonts.Saeum0};
-  margin-bottom: 1rem;
-`;
-
-const SubText = styled.div`
-  color: ${({ theme }) => theme.colors.Gray4};
-  ${({ theme }) => theme.fonts.Body2};
-  text-align: center;
-`;
+export default CompletePage;
