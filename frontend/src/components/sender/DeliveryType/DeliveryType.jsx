26bf7146f8b910cea3a9af1dd1e5e007a77b0cc3
hnl9912 <hnl9912@naver.com>
2025-03-28 09:58:02 +0900

    feat: 엽서 전송 API 연결

--- frontend/src/components/sender/DeliveryType/DeliveryType.jsx
+++ frontend/src/components/sender/DeliveryType/DeliveryType.jsx
@@ -1,102 +1,3 @@
-// // src/components/sender/DeliveryType/DeliveryType.jsx
-// import React from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import { useRecoilValue } from 'recoil';
-// import styled from 'styled-components';
-
-// import { sendPostcard } from '../../../apis/postcard';
-// import IconLetter from '../../../assets/images/sender/NormalOption.png';
-// import IconTimer from '../../../assets/images/sender/OpenTimerOption.png';
-// import IconCalendar from '../../../assets/images/sender/ScheduledOption.png';
-// import IconLock from '../../../assets/images/sender/SecretOption.png';
-// import { PostcardImageFile, PostcardText, SelectedKeyringId } from '../../../recoil/atom';
-// import Header from '../../common/Header';
-// import QuestionText from '../SelectDear/QuestionText';
-// import DeliveryTypeCard from './DeliveryTypeCard';
-
-// const DeliveryType = () => {
-//   const navigate = useNavigate();
-//   const selectedKeyringId = useRecoilValue(SelectedKeyringId);
-//   const postcardImageFile = useRecoilValue(PostcardImageFile);
-//   const postcardText = useRecoilValue(PostcardText);
-
-//   const deliveryTypes = [
-//     {
-//       icon: IconLetter,
-//       title: '일반 편지',
-//       description: '바로 전송됩니다!',
-//       value: 'NORMAL',
-//     },
-//     {
-//       icon: IconTimer,
-//       title: '오픈 타이머',
-//       description: '타이머 종료 후 읽을 수 있습니다!',
-//       value: 'TIMER',
-//     },
-//     {
-//       icon: IconLock,
-//       title: '비밀 편지',
-//       description: '정답을 맞추면 열리는 편지입니다!',
-//       value: 'LOCKED',
-//     },
-//     {
-//       icon: IconCalendar,
-//       title: '예약 편지',
-//       description: '예약된 날짜 및 시간에 발송합니다!',
-//       value: 'SCHEDULED',
-//     },
-//   ];
-
-//   const handleSelect = (type) => {
-//     console.log('선택된 전송 방식:', type);
-//     // 추후: Recoil 저장 또는 페이지 이동
-//   };
-
-//   return (
-//     <Wrapper>
-//       <Header headerName="전송유형선택" />
-//       <QuestionTextWrapper>
-//         <QuestionText text="전송 방식을 선택해주세요!" />
-//       </QuestionTextWrapper>
-
-//       <CardList>
-//         {deliveryTypes.map((type) => (
-//           <DeliveryTypeCard
-//             key={type.value}
-//             icon={type.icon}
-//             title={type.title}
-//             description={type.description}
-//             onClick={() => handleSelect(type.value)}
-//           />
-//         ))}
-//       </CardList>
-//     </Wrapper>
-//   );
-// };
-
-// export default DeliveryType;
-
-// const Wrapper = styled.div`
-//   position: relative;
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   justify-content: space-between;
-//   width: 100%;
-//   height: auto;
-// `;
-
-// const CardList = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   gap: 1.2rem;
-// `;
-// const QuestionTextWrapper = styled.div`
-//   margin-top: 10rem;
-//   margin-bottom: 2rem;
-//   display: flex;
-//   justify-content: center;
-// `;
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
@@ -163,15 +64,12 @@ const DeliveryType = () => {
         content: postcardText,
       };
 
-      const formData = new FormData();
-      formData.append('postcard', JSON.stringify(postcardData));
-      formData.append('image', postcardImageFile);
-
-      console.log('📦 전송할 postcard:', postcardData);
-      console.log('🖼️ 이미지 파일:', postcardImageFile);
-
       try {
-        const result = await sendPostcard(formData);
+        await sendPostcard({
+          postcardData,
+          imageFile: postcardImageFile,
+        });
+
         alert('엽서가 전송되었습니다!');
         navigate('/complete');
       } catch (error) {
@@ -186,7 +84,10 @@ const DeliveryType = () => {
   return (
     <Wrapper>
       <Header headerName="전송유형선택" />
-      <QuestionText text="어떤 방식으로 보낼까요?" />
+      <QuestionTextWrapper>
+        <QuestionText text="전송 방식을 선택해주세요!" />
+      </QuestionTextWrapper>
+
       <CardList>
         {deliveryTypes.map((type) => (
           <DeliveryTypeCard
@@ -205,14 +106,23 @@ const DeliveryType = () => {
 export default DeliveryType;
 
 const Wrapper = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: auto;
 `;
 
 const CardList = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1.2rem;
-  padding: 0 2rem;
-  margin-top: 3rem;
+`;
+const QuestionTextWrapper = styled.div`
+  margin-top: 10rem;
+  margin-bottom: 2rem;
+  display: flex;
+  justify-content: center;
 `;
