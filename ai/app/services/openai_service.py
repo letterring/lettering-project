1bc0b5eb1721f7484a6e8946beda0980437c9cac
0ys <0ysgong@gmail.com>
2025-04-01 16:40:26 +0900

    feat: env 환경변수 호출 settings 함수로 관리

--- ai/app/services/openai_service.py
+++ ai/app/services/openai_service.py
@@ -1,11 +1,7 @@
-import os
+from app.core.settings import settings
 from openai import OpenAI
-from dotenv import load_dotenv
 
-load_dotenv()
-# print("🔑", os.getenv("OPENAI_API_KEY"))
-
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key=settings.OPENAI_API_KEY)
 
 def chat_with_gpt(prompt: str) -> str:
     response = client.chat.completions.create(
