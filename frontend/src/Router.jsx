98de4fcc8761b3aa5ff57fb826f7f4f3d86bea27
유하람 <dlfandl97@naver.com>
2025-04-02 13:04:36 +0900

    feat: 받는사람 편지 상세 api 연결

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -81,7 +81,7 @@ const Router = () => {
               <Route path="/dear/postcard/:messageId" element={<DearPostcardPage />} />
               <Route path="/dear/postcard/detail/:messageId" element={<DearPostDetailPage />} />
               <Route path="/dear/letter" element={<DearLetterPage />} />
-              <Route path="/dear/letter/detail" element={<DearLetterDetailPage />} />
+              <Route path="/dear/letter/detail/:messageId" element={<DearLetterDetailPage />} />
               <Route path="/dear/mailbox" element={<DearMailBoxPage />} />
 
               {/* common */}
