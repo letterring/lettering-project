a6204d7539edff0f1b7e6ce404641d46c3a11e18
유하람 <dlfnadl97@naver.com>
2025-04-03 09:37:20 +0900

    feat: 보내는 사람 편지 상세

--- frontend/src/apis/letter.js
+++ frontend/src/apis/letter.js
@@ -18,3 +18,12 @@ export const getLetterDetail = async (messageId) => {
     console.error('편지 상세정보 조회에 실패했습니다.', err);
   }
 };
+
+export const getSentLetterDetail = async (messageId) => {
+  try {
+    const { data } = await client.get(`/messages/letters/sender/${messageId}`);
+    return data;
+  } catch (err) {
+    console.error('편지 상세정보 조회에 실패했습니다.', err);
+  }
+};
