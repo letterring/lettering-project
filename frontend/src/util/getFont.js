df299bae91ca4575ab48aa91e3d5af48759e9802
0ys <0ysgong@gmail.com>
2025-04-10 09:57:08 +0900

    feat: 폰트 수정 및 싸피 엽서 관련 QA

--- frontend/src/util/getFont.js
+++ frontend/src/util/getFont.js
@@ -1,8 +1,11 @@
 const fontList = [
-  { id: 'GOMSIN1', name: 'Gomsin', style: 'Gomsin2' },
+  { id: 'GOMSIN1', name: 'Gomsin', style: 'Gomsin1' },
+  { id: 'GOMSIN2', name: 'Gomsin', style: 'Gomsin2' },
   { id: 'UHBEE1', name: 'Uhbee BEOJJI', style: 'Uhbee1' },
   { id: 'SAEUM5', name: 'GangwonEduSaeeum', style: 'Saeum5' },
+  { id: 'SAEUM6', name: 'GangwonEduSaeeum', style: 'Saeum6' },
   { id: 'EDUBODY0', name: 'GangwonEduAll', style: 'EduBody0' },
+  { id: 'EDUBODY1', name: 'GangwonEduAll', style: 'EduBody1' },
 ];
 
 export const getFontStyle = (id) => {
